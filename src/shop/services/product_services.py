4234c5422d320f8c02bfe580a8f9abcce33d73a0
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-02 16:39:39 +0600

    Исправлена сортировка а также добавлены ссылки для переходу с имени категории

--- src/shop/services/product_services.py
+++ src/shop/services/product_services.py
@@ -1,12 +1,13 @@
 from ..models import Product
 from django.shortcuts import get_object_or_404
+from django.db.models.functions import Lower
 
 def get_product_list():
-    product_list = Product.objects.in_stock().order_by('category','name')
+    product_list = Product.objects.in_stock().order_by('category',Lower('name'))
     return product_list
 
 def get_product_by_pk(pk):
     return get_object_or_404(Product, pk=pk)
 
 def get_product_list_by_category(category):
-    return Product.objects.in_stock().filter(category=category)
+    return Product.objects.in_stock().filter(category=category).order_by(Lower('name'))
